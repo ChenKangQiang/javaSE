ea4f6be3b4619b5bfcfba30428a75e3439ea475c
ChenKangQiang <ckq123@outlook.com>
2017-07-12 23:31:21 +0800

    动态代理demo

---
+++ src/main/java/edu/tongji/comm/javase/dynamicproxy/RealSubject.java
@@ -0,0 +1,17 @@
+package edu.tongji.comm.javase.dynamicproxy;
+
+/**
+ * Created by chen on 2017/7/12.
+ */
+public class RealSubject implements Subject {
+
+    @Override
+    public void rent() {
+
+    }
+
+    @Override
+    public void hello(String str) {
+
+    }
+}
---
+++ src/main/java/edu/tongji/comm/javase/dynamicproxy/Subject.java
@@ -0,0 +1,11 @@
+package edu.tongji.comm.javase.dynamicproxy;
+
+/**
+ * Created by chen on 2017/7/12.
+ */
+public interface Subject {
+
+    void rent();
+
+    void hello(String str);
+}
