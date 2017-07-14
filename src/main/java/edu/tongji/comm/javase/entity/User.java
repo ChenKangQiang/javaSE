26c3bac0cc3bdb1ee7d9425e1b57babe9144cfbe
chenkangqiang <chenkangqiang@meituan.com>
2017-07-14 16:03:30 +0800

    add files

---
+++ src/main/java/edu/tongji/comm/javase/entity/User.java
@@ -0,0 +1,14 @@
+package edu.tongji.comm.javase.entity;
+
+import lombok.Data;
+
+/**
+ * Created by chen on 2017/7/14.
+ */
+
+@Data
+public class User {
+    private Integer userID;
+    private String username;
+    private String password;
+}
