bece1dde5e6b5e1e0ef138510ed469201f4d3d94
chenkangqiang <chenkangqiang@meituan.com>
2017-07-31 11:46:07 +0800

    add files

--- src/main/java/edu/tongji/comm/javase/json/JsonTest.java
+++ src/main/java/edu/tongji/comm/javase/json/JsonTest.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import edu.tongji.comm.javase.entity.User;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,11 +29,13 @@ public class JsonTest {
 
         List<Integer> list1 = new ArrayList<>(Arrays.asList(10, 9, 8, 7, 5));
         List<Integer> list2 = Lists.newArrayList();
+//        List<Integer> list3 = Arrays.asList(10, 9, 8, 7, 5);
         list2 = Lists.newArrayList();
         list2 = new ArrayList<>();
 
         System.out.println(JSON.toJSONString(method(list1, 2)));
         System.out.println(JSON.toJSONString(method(list2, 2)));
+//        System.out.println(JSON.toJSONString(method(list3, 2)));
     }
 
 
