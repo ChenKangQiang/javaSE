bb5cd57370f501ca7355a8ddeda9f2d2ecfb955a
chenkangqiang <chenkangqiang@meituan.com>
2017-09-01 12:00:14 +0800

    享元模式

---
+++ src/main/java/edu/tongji/comm/design/pattern/flyweight/BlackIgoChessman.java
@@ -0,0 +1,13 @@
+package edu.tongji.comm.design.pattern.flyweight;
+
+/**
+ * @author chenkangqiang
+ * @date 2017/9/1
+ */
+public class BlackIgoChessman extends IgoChessman {
+
+    @Override
+    public String getColor() {
+        return "黑色";
+    }
+}
