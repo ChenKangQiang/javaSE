3124f44fa993989fb27a8fdf3be325bf6c834f03
chenkangqiang <chenkangqiang@meituan.com>
2017-09-11 16:13:51 +0800

    一些例子

---
+++ src/main/java/edu/tongji/comm/bigdecimal/Test.java
@@ -0,0 +1,40 @@
+package edu.tongji.comm.bigdecimal;
+
+import com.alibaba.fastjson.JSON;
+
+import java.math.BigDecimal;
+
+/**
+ * @author chenkangqiang
+ * @date 2017/9/11
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Long long1 = new Long(12345678);
+        Long long2 = new Long(12345678);
+        BigDecimal bigDecimal = new BigDecimal(13.9);
+        BigDecimal bigDecimal1 = new BigDecimal(12.1);
+        BigDecimal bigDecimal2 = new BigDecimal(11);
+        //最好以String的方式来初始化BigDecimal
+        BigDecimal bigDecimal3 = new BigDecimal("12.999");
+        String str1 = long1 + "";
+        String str2 = long2 + "";
+        String str3 = bigDecimal + "";
+        System.out.println(bigDecimal);
+//        System.out.println(bigDecimal.setScale(1, BigDecimal.ROUND_DOWN));
+//        System.out.println(bigDecimal1);
+//        System.out.println(bigDecimal1.setScale(1, BigDecimal.ROUND_DOWN));
+//        System.out.println(str3);
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.equals("12345678"));
+//        System.out.println(str3.equals("13.9"));
+        System.out.println(bigDecimal.scale());
+        System.out.println(bigDecimal.setScale(1, BigDecimal.ROUND_HALF_DOWN));
+        System.out.println(bigDecimal1.setScale(1, BigDecimal.ROUND_HALF_DOWN));
+        System.out.println(bigDecimal2.setScale(1, BigDecimal.ROUND_HALF_DOWN).intValueExact());
+        System.out.println(bigDecimal3.toString());
+    }
+
+
+}
